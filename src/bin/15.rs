0fe0c3be928e444d0284245f97255daab4b85107
AmirMohammad Pirzadeh <am.pirzade@gmail.com>
2024-02-02 21:57:06 +0000

    problem 15

---
+++ src/bin/15.rs
@@ -0,0 +1,8 @@
+/**
+ * The question is the same question as the number of ways you can creating a string of letters of R and D for right and down instructions
+ * The number of such permutations is
+ *      (20+20)!        40!
+ *     ---------- = ---------- = 137846528820
+ *      20!*20!       20!*20!
+ */
+fn main() {}
