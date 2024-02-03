2f95a239ae763eb1c4adb96bafe2180cd087d321
AmirMohammad Pirzadeh <am.pirzade@gmail.com>
2024-02-03 17:24:53 +0000

    problem 22

---
+++ src/bin/22.rs
@@ -0,0 +1,26 @@
+use std::fs;
+
+fn get_score(name: &str) -> usize {
+    let mut score = 0;
+    for c in name.chars() {
+        score += c as usize - 64;
+    }
+    score
+}
+
+fn main() -> std::io::Result<()> {
+    // ! Provide file
+    let file_contents = fs::read_to_string("./src/bin/names.txt").unwrap();
+
+    let all_names = file_contents.replace(r#"""#, "");
+    let mut all_names: Vec<&str> = all_names.split(",").collect();
+    all_names.sort();
+    
+    let mut score_sum = 0;
+    for (i, name) in all_names.iter().enumerate() {
+        score_sum += get_score(name) * (i + 1);
+    }
+    println!("{:?} {}", all_names, score_sum);
+
+    Ok(())
+}
