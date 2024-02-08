be7597a5cdd459b14faa6d8e3702dc196fe9558f
AmirMohammad Pirzadeh <am.pirzade@gmail.com>
2024-02-08 16:57:19 +0000

    problem 42

---
+++ src/bin/42.rs
@@ -0,0 +1,26 @@
+use std::fs;
+
+fn is_triangle(x: u64) -> bool {
+    let n = ((8 * x + 1) as f64).sqrt();
+    return n == n.trunc();
+}
+
+fn main() {
+    // ! Provide file
+    let file_contents = fs::read_to_string("./src/bin/words.txt").unwrap();
+
+    let words = file_contents.replace(r#"""#, "");
+    let words: Vec<&str> = words.split(",").collect();
+    let mut sum;
+    let mut cnt = 0;
+    for word in words {
+        sum = 0;
+        for letter in word.chars() {
+            sum += letter as u64 - 64;
+        }
+        if is_triangle(sum) {
+            cnt += 1;
+        }
+    }
+    println!("{cnt}");
+}
